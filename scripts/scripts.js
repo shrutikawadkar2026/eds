6782aeaaf78abe4d8f8b52454422c28cdab393c7
Shruti Kawadkar <shruti.k.infodales@gmail.com>
2026-06-18 13:56:55 +0530

    css change

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -183,3 +183,9 @@ async function loadPage() {
 }
 
 loadPage();
+
+(async function loadDa() {
+  if (!new URL(window.location.href).searchParams.get('dapreview')) return;
+  import('https://da.live/scripts/dapreview.js').then(({ default: daPreview }) => daPreview(loadPage));
+}());
+
